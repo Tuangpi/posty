9f2b650c9dfe8dd3ec1cb402348873077fdfaae3
SingTuang <singkhantuang97@gmail.com>
2021-01-18 17:41:53 +0630

    second commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Posty</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
+    <div></div>
     @yield('content')
 </body>
 </html>
