6829810912fe3599c10830e151baefbf7bb72fce
SingTuang <singkhantuang97@gmail.com>
2021-01-18 22:01:59 +0630

    posts

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,26 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginContoller;
+use App\Http\Controllers\Auth\LogoutController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PostController;
 
-Route::get('/dashboard',[DashboardController::class, 'index'])->name('dashboard');
+Route::get('/', function(){
+    return view('home');
+})->name('home');
 
-Route::get('/register',[RegisterController::class, 'index'])->name('register');
+Route::get('/dashboard',[DashboardController::class, 'index'])
+->name('dashboard');
+
+Route::post('/logout',[LogoutController::class,'store'])->name('logout');
+
+Route::get('/login',[LoginContoller::class,'index'])->name('login');
+Route::post('/login',[LoginContoller::class, 'store']);
+
+Route::get('/register',[RegisterController::class, 'store'])->name('register');
 Route::post('/register',[RegisterController::class, 'store']);
 
-Route::get('/', function () {
-    return view('posts.index');
-});
+Route::get('/post',[PostController::class,'index'])->name('posts');
+Route::post('/post',[PostController::class,'store']);
